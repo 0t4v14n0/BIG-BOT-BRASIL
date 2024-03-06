9f6d1480676fd91d8e72473bd1b3fca338fe3ee1
Otaviano Cruz Gouveia de Arruda Neto <58171774+0t4v14n0@users.noreply.github.com>
2024-03-06 04:07:49 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -24,78 +24,77 @@ class meuapp:
 
         self.tab_control = ttk.Notebook(self.root)
 
-        # Agora crie as abas
+        #criacoes de abas
         self.tab1 = ttk.Frame(self.tab_control)
         self.tab2 = ttk.Frame(self.tab_control)
 
-        # Adicione as abas ao controlador de abas
+        # adiciona as abas ao controlador
         self.tab_control.add(self.tab1, text='BOT-BBB')
         self.tab_control.add(self.tab2, text='Posicao')
 
-        # Adicione o controlador de abas à janela raiz
+        # adicione o controlador de abas a janela raiz
         self.tab_control.pack(expand=1, fill="both")
 
-        # Widget Label para exibir a imagem de fundo na aba 1
+        # label para exibir a imagem de fundo na aba 1
         self.label_fundo_tab1 = Label(self.tab1, image=self.img_menuprincipal)
         self.label_fundo_tab1.place(x=0, y=0, relwidth=1, relheight=1)
 
-        # Widget Label para exibir a imagem de fundo na aba 2
+        # label para exibir a imagem de fundo na aba 2
         self.label_fundo_tab2 = Label(self.tab2, image=self.img_menuprincipal2)
         self.label_fundo_tab2.place(x=0, y=0, relwidth=1, relheight=1)
 
         # widgets
-        #self.lbl1 = Label(self.tab1, text='bot bbb')
-        #self.lbl1.grid(column=0, row=0)
+
+        #aba 1
         
         #primeira
 
-        label_p = tk.Label(self.tab1, text="posicao botao 1")
-        label_p.place(x=50, y=105)
-        label_x = tk.Label(self.tab1, text="X:")
-        label_x.place(x=50, y=130)
-        entry_x = tk.Entry(self.tab1, width=4)
-        entry_x.place(x=70, y=130)
+        self.label_p = tk.Label(self.tab1, text="posicao botao 1")
+        self.label_p.place(x=50, y=105)
+        self.label_x = tk.Label(self.tab1, text="X:")
+        self.label_x.place(x=50, y=130)
+        self.entry_x = tk.Entry(self.tab1, width=4)
+        self.entry_x.place(x=70, y=130)
 
-        label_y = tk.Label(self.tab1, text="Y:")
-        label_y.place(x=100, y=130)
-        entry_y = tk.Entry(self.tab1, width=4)
-        entry_y.place(x=120, y=130)
+        self.label_y = tk.Label(self.tab1, text="Y:")
+        self.label_y.place(x=100, y=130)
+        self.entry_y = tk.Entry(self.tab1, width=4)
+        self.entry_y.place(x=120, y=130)
 
         #segunda
 
-        label_s = tk.Label(self.tab1, text="posicao botao 2")
-        label_s.place(x=50, y=163)
-        label_x1 = tk.Label(self.tab1, text="X:")
-        label_x1.place(x=50, y=188)
-        entry_x1 = tk.Entry(self.tab1, width=4)
-        entry_x1.place(x=70, y=188)
+        self.label_s = tk.Label(self.tab1, text="posicao botao 2")
+        self.label_s.place(x=50, y=163)
+        self.label_x1 = tk.Label(self.tab1, text="X:")
+        self.label_x1.place(x=50, y=188)
+        self.entry_x1 = tk.Entry(self.tab1, width=4)
+        self.entry_x1.place(x=70, y=188)
 
-        label_y1 = tk.Label(self.tab1, text="Y:")
-        label_y1.place(x=100, y=188)
-        entry_y1 = tk.Entry(self.tab1, width=4)
-        entry_y1.place(x=120, y=188)
+        self.label_y1 = tk.Label(self.tab1, text="Y:")
+        self.label_y1.place(x=100, y=188)
+        self.entry_y1 = tk.Entry(self.tab1, width=4)
+        self.entry_y1.place(x=120, y=188)
 
         #terceira
 
-        label_t = tk.Label(self.tab1, text="posicao botao 3")
-        label_t.place(x=50, y=222)
-        label_x2 = tk.Label(self.tab1, text="X:")
-        label_x2.place(x=50, y=247)
-        entry_x2 = tk.Entry(self.tab1, width=4)
-        entry_x2.place(x=70, y=247)
+        self.label_t = tk.Label(self.tab1, text="posicao botao 3")
+        self.label_t.place(x=50, y=222)
+        self.label_x2 = tk.Label(self.tab1, text="X:")
+        self.label_x2.place(x=50, y=247)
+        self.entry_x2 = tk.Entry(self.tab1, width=4)
+        self.entry_x2.place(x=70, y=247)
 
-        label_y2 = tk.Label(self.tab1, text="Y:")
-        label_y2.place(x=100, y=247)
-        entry_y2 = tk.Entry(self.tab1, width=4)
-        entry_y2.place(x=120, y=247)
+        self.label_y2 = tk.Label(self.tab1, text="Y:")
+        self.label_y2.place(x=100, y=247)
+        self.entry_y2 = tk.Entry(self.tab1, width=4)
+        self.entry_y2.place(x=120, y=247)
 
         #botao do envio do forms
 
-        submit_button = tk.Button(self.tab1, text="Enviar", command= self.script)
+        submit_button = tk.Button(self.tab1, text="INICIAR", command= self.script)
         submit_button.place(x=60, y=300,width=70,height=20)
 
-        #self.lbl2 = Label(self.tab2, text='posicao do mouse')
-        #self.lbl2.grid(column=0, row=0)
+        #aba 2
 
         self.lbl2 = tk.Button(self.tab2,bd=0,image=self.img_botao ,command=self.local)
         self.lbl2.place(x=48,y=142,width=150,height=40)
@@ -109,9 +108,35 @@ class meuapp:
         a = str(pyautogui.position())
         self.texto.config(text="Valor: " + a)
 
-    #-------------------------------local---------------------------------
+    #-----------------------------script---------------------------------\
+    
     def script(self):
-        print("ola")
+            #pega os valores do formulario
+            x =  int(self.entry_x.get())
+            y =  int(self.entry_y.get())
+            x1 = int(self.entry_x1.get())
+            y1 = int(self.entry_y1.get())
+            x2 = int(self.entry_x2.get())
+            y2 = int(self.entry_y2.get())
+
+            #array com as cordenadas
+            posicoes = [(x, y), (x1, y1), (x2, y2)]
+
+            #numero de cliqyues do mouse
+            num_cliques_por_posicao = 1
+
+            #repeticoes infinitas 
+            while True:
+                 #vai percorrer todos os valores do array
+                 for posicao in posicoes:
+                    #mover o mouse nas cordenas
+                    pyautogui.moveTo(posicao[0], posicao[1], duration=1)
+
+                    for _ in range(num_cliques_por_posicao):
+                        pyautogui.click()
+
+                        time.sleep(1)
+
          
 #-----------------------------------MAIN-------------------------------
 
